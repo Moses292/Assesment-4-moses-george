555b68c5bf36a48e97fa17ab29f88670885b98c4
Agent292 <email.georgemoses@292.gmail.com>
2026-05-27 14:11:54 +1100

    api.js modified

--- api.js
+++ api.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const visiteur = process.env.visiteur || 'Anonyme';
 
-app.get('/', (req, res) => {
-  res.send('you are the best ');
+app.get('/health', (req, res) => {
+  res.json({
+    status: "ok",
+    environment: process.env.APP_ENV || "local"
+  });
 });
 
 app.get('/api', (req, res) => {
